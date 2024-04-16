ff9d8acf145e66bbacf547f1bba74f520940dc41
Rohit Rai <rohitrai19979090@gmail.com>
2024-04-16 14:17:03 +0530

    data setup

---
+++ demo.java
@@ -0,0 +1,11 @@
+package DSA;
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println("Hey Input : " + x);
+        scanner.close();
+    }
+}
