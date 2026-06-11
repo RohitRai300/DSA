064ee1a087a3f0e28bea4bab3b9dfa45ad36a173
Rohit Rai <rohitrai19979090@gmail.com>
2026-06-12 00:01:21 +0530

    commit patterns 1

---
+++ 1-Patterns/Pattern1.cpp
@@ -0,0 +1,37 @@
+/*
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+class Pattern1 {
+public:    
+
+void printPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+int main() {
+    cout << "Enter the number of rows: ";
+    int n;
+    cin >> n;
+
+    Pattern1 pattern;
+    pattern.printPattern(n);
+    return 0;
+}
